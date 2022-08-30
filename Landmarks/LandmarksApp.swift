baafa89e732d6ba8a68ef6bab6c82d9e15f73016
ismaelsousa <ismaelsousa1998@outlook.com>
2022-08-30 20:50:44 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ismael Sousa on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
