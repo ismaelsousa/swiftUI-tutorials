0e6907b7b8c7eb7383edcbdf7eaa28db972831b4
ismaelsousa <ismaelsousa1998@outlook.com>
2022-09-04 13:45:35 -0300

    interfacing with UIKit

---
+++ Landmarks/Views/PageView/PageView.swift
@@ -0,0 +1,33 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Ismael Sousa on 04/09/22.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    
+    var body: some View {
+          ZStack(alignment: .bottomTrailing) {
+              PageViewController(pages: pages, currentPage: $currentPage)
+              PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                  .frame(width: CGFloat(pages.count * 18))
+                  .padding(.trailing)
+          }
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(
+            pages: ModelData().featureds.map({ landmark in
+                FeatureCard(landmark: landmark)
+                    .aspectRatio(3/2, contentMode: .fit)
+            })
+        )
+    }
+}
